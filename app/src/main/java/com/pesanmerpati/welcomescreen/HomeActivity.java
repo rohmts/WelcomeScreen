b2aa44cd064eac1fc590856e261b7758b9bec1c0
rohmts <rohmatsuseno@gmail.com>
2018-06-25 19:40:54 +0700

    Add Welcome & Home screen

--- app/src/main/java/com/pesanmerpati/welcomescreen/HomeActivity.java
+++ app/src/main/java/com/pesanmerpati/welcomescreen/HomeActivity.java
@@ -1,17 +1,13 @@
 package com.pesanmerpati.welcomescreen;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-        finish();
+        setContentView(R.layout.activity_home);
     }
 }
